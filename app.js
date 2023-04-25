7d8e285a8afb07c99e2c049f3b1c55ea80f7d673
Rahul Ranjan <rahulranjan@Rahuls-MacBook-Pro.local>
2023-04-25 08:06:02 +0530

    Start command updated

--- app.js
+++ app.js
@@ -21,8 +21,6 @@ db.once("open", () => {
   console.log("Connected correctly to server");
 });
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
